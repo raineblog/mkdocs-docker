bd264a8b08ff3af6da67cb0b655e99b28b4278a0
RainPPR <PPR2125773894@163.com>
2026-01-11 18:55:00 +0800

    Update

---
+++ scripts/generate.serve.py
@@ -0,0 +1,42 @@
+import yaml
+import json
+import os
+from pathlib import Path
+
+script_dir = Path(__file__).parent.resolve()
+
+def parse_yaml(yaml_path):
+    with open(yaml_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+    return yaml.load(text, Loader=yaml.FullLoader)
+
+def load_json(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+intro = [
+    {
+        '简介': [
+            'index.md',
+            'intro/format.md',
+            'intro/usage.md',
+            'intro/discussion.md',
+            'madoka.md'
+        ]
+    }
+]
+
+def get_site_template():
+    info = load_json('info.json')
+    template = info['project']
+    template = template | parse_yaml(os.path.join(script_dir, "template.serve.yml"))
+    template = template | parse_yaml('docs/assets/extra.yml')
+    template['extra'] = info['extra']
+    template['nav'] = intro + [{item['title']: item['children']} for item in info['nav']]
+    del(template['site_url'])
+    return template
+
+if __name__ == "__main__":
+    with open('mkdocs.yml', 'w', encoding='utf-8') as file:
+        yaml.dump(get_site_template(), file, allow_unicode=True, indent=4, sort_keys=False)
